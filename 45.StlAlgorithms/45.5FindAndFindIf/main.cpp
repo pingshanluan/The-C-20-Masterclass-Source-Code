de76e37ee2465136866b58f72b8db4c7a834b45f
pingshan <pingshan.luan@us.tel.com>
2023-06-15 20:08:23 -0400

    daily commit, 06-15-2023

--- 45.StlAlgorithms/45.5FindAndFindIf/main.cpp
+++ 45.StlAlgorithms/45.5FindAndFindIf/main.cpp
@@ -2,6 +2,10 @@
 #include <algorithm>
 #include <vector>
 
+// find things in collections
+// std::find(it_begin, it_end, n);      //find n, if found return an iterator to the first element in the range, if not return iterator pointing to the past-the-end
+// std::find_if(it_begin, it_end, predicate);      //if predicate is true, return an iterator to the first element in the range that satisfies the predicate, if false return iterator pointing to the past-the-end
+
 int main(){
 
     //int n = 23;
@@ -9,16 +13,19 @@ int main(){
  
     std::vector<int> collection{14, 24,72, 8, 98, 112};
 
+    //returns iterator rathan than value
     auto result = std::find(std::begin(collection), std::end(collection), n);
  
     if (result != std::end(collection)) {
-        std::cout << "collection contains: " << n << std::endl;
+        std::cout << "collection contains: " << *result << std::endl;
     } else {
         std::cout << "collection does not contain: " << n << std::endl;
     }
 
     std::cout << "------------" << std::endl;
 
+    //find_if
+    //prdicate
     auto odd = [](int x){
         if( (x%2) != 0)
             return true;
